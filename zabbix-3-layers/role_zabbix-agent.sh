f38d04131f89d115340b87c15a5c7c86b03d163a
Aguinaldo S Freitas <aguynaldo@gmail.com>
2020-07-15 00:54:20 -0300

    Adding settings of zabbix-proxy server.

--- zabbix-3-layers/role_zabbix-agent.sh
+++ zabbix-3-layers/role_zabbix-agent.sh
@@ -7,14 +7,21 @@ read_var() {
     echo ${VAR[1]}
 }
 
-ZABBIX_SRV_IP=$(read_var ZABBIX_SRV_IP /vagrant/.env)
+PROXY01_SRV_IP=$(read_var PROXY01_SRV_IP /vagrant/.env)
+ZABBIX_HOSTNAME=$(read_var ZABBIX_HOSTNAME /vagrant/.env)
+PROXY01_HOSTNAME=$(read_var PROXY01_HOSTNAME /vagrant/.env)
+
+host_name=$(hostname)
 
 echo "[TASK 1] Install Zabbix Agent."; sleep 3
 dnf install -y zabbix-agent
 
 echo "[TASK 2] Editing Zabbix Agent conf."; sleep 3
-sed -i 's/^Server=127.0.0.1$/Server='$ZABBIX_SRV_IP'/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/^ServerActive=127.0.0.1$/ServerActive='$ZABBIX_SRV_IP'/' /etc/zabbix/zabbix_agentd.conf
+# Mantem o ip de loopback para os zabbix-server e zabbix-proxy.
+if [ $host_name != $ZABBIX_HOSTNAME ] && [ $host_name != $PROXY01_HOSTNAME ]; then
+    sed -i 's/^Server=127.0.0.1$/Server='$PROXY01_SRV_IP'/' /etc/zabbix/zabbix_agentd.conf
+    sed -i 's/^ServerActive=127.0.0.1$/ServerActive='$PROXY01_SRV_IP'/' /etc/zabbix/zabbix_agentd.conf
+fi
 sed -i 's/Hostname=Zabbix server/# Hostname=Zabbix Server/' /etc/zabbix/zabbix_agentd.conf
 sed -i 's/# HostnameItem=system.hostname/HostnameItem=system.hostname/' /etc/zabbix/zabbix_agentd.conf
 
---
+++ zabbix-3-layers/role_zabbix-proxy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+### Function to read the constants of the .env file
+read_var() {
+    VAR=$(grep $1 $2 | xargs)
+    IFS="=" read -ra VAR <<< "$VAR"
+    echo ${VAR[1]}
+}
+
+ZABBIX_SRV_IP=$(read_var ZABBIX_SRV_IP /vagrant/.env)
+
+echo "[TASK 1] Install Zabbix Proxy."; sleep 3
+dnf install -y zabbix-proxy-sqlite3 zabbix-get zabbix-sender
+
+echo "[TASK 2] Create directory and set permissions for sqlite.db file."; sleep 3
+mkdir -p /var/lib/zabbix
+chown zabbix. -R /var/lib/zabbix
+
+
+echo "[TASK 3] Editing zabbix_proxy.conf file"; sleep 3
+cp /etc/zabbix/zabbix_proxy.conf /etc/zabbix/zabbix_proxy.conf_original
+sed -i 's/^Server=127.0.0.1$/Server='$ZABBIX_SRV_IP'/' /etc/zabbix/zabbix_proxy.conf
+sed -i 's/^ServerActive=127.0.0.1$/ServerActive='$ZABBIX_SRV_IP'/' /etc/zabbix/zabbix_proxy.conf
+sed -i 's/Hostname=Zabbix proxy/# Hostname=Zabbix proxy/' /etc/zabbix/zabbix_proxy.conf
+sed -i 's/# HostnameItem=system.hostname/HostnameItem=system.hostname/' /etc/zabbix/zabbix_proxy.conf
+sed -i 's+DBName=zabbix_proxy+DBName=/var/lib/zabbix/zabbix_proxy+' /etc/zabbix/zabbix_proxy.conf
+sed -i 's/# ConfigFrequency=3600/ConfigFrequency=300/' /etc/zabbix/zabbix_proxy.conf
+
+
+echo "[TASK 4] Setting firewall rules"
+firewall-cmd --permanent --add-port=10051/tcp
+firewall-cmd --reload
+
+echo "[TASK 5] Enable and starting zabbix-agent service"; sleep 3
+systemctl enable --now zabbix-proxy
+systemctl restart zabbix-proxy
+
+
